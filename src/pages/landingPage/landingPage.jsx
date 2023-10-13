30daea135a523c59c65379f0613677686fdc3fa1
BRun0442 <brunoosmar442@gmail.com>
2023-10-13 01:03:19 -0300

    Background

---
+++ src/pages/landingPage/landingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./landingPage.css";
+
+function landingPage() {
+  return (
+    <section className='background'>
+      <div className='background-image' />
+
+      <div style={{position: "relative", zIndex: 1}}>
+        <section className='about'>
+          
+        </section>
+      </div>
+    </section>
+  )
+}
+
+export default landingPage
