7e93ce3e255e5ff0da591813dd4175eae4f4da79
BRun0442 <brunoosmar442@gmail.com>
2024-01-11 18:22:31 -0300

    landingPage

--- src/pages/landingPage/landingPage.jsx
+++ src/pages/landingPage/landingPage.jsx
@@ -1,15 +1,63 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 import "./landingPage.css";
+
+const linkBackground = {
+  backgroundColor: '#219C90',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+
+  paddingLeft: '0.6rem',
+  paddingRight: '0.6rem'
+}
+
+const linkBorder = {
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+
+  borderLeft: '2px solid #219C90',
+  borderRight: '2px solid #219C90',
+
+  paddingLeft: '1.4rem',
+  paddingRight: '1.4rem'
+}
 
 function landingPage() {
   return (
     <section className='background'>
       <div className='background-image' />
 
-      <div style={{position: "relative", zIndex: 1}}>
-        <section className='about'>
-          
-        </section>
+      <div className='content'>
+        <header className='header'>
+          <h1 className='headerTitle'>PePets</h1>
+
+          <div className='linkHeader'>
+            <Link className='lightFontHeader'>Início</Link>
+            <Link className='lightFontHeader'>Animais Perdidos</Link>
+            <div style={linkBackground}><Link className='boldFontHeader'>Anunciar</Link></div>
+            <div style={linkBorder}><Link className='lightFontHeader'>Login</Link></div>
+          </div>
+        </header>
+
+        <main>
+          <div className='mainText'>
+            <h1 className='mainTitle'>PePets</h1>
+
+            <p>
+              No PePets, oferecemos um espaço virtual para unir esforços na 
+              <spam style={{fontWeight: 600}}> busca por pets desaparecidos </spam>,
+              promovendo a solidariedade entre amantes de animais e compartilhando informações cruciais
+              para reunir famílias com seus queridos companheiros.
+            </p>
+          </div>
+
+          <div className='mainPhrase'>
+            <h2>Divulgue aqui seu pet perdido!</h2>
+          </div>
+        </main>
       </div>
     </section>
   )
